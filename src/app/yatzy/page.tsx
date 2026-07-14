314c8be035d12ddb4d274cf60b63b6e6d4a6bfec
Jan Sander <sander.jan@gmx.net>
2026-07-14 23:37:26 +0200

    Remove card

--- src/app/yatzy/page.tsx
+++ src/app/yatzy/page.tsx
@@ -2,13 +2,11 @@
 
 import { gamemodes } from "@/components/gamemodes/gamemodes";
 import { usePlayerRoster } from "@/components/hooks/usePlayerRoster";
-import { Card } from "@/components/ui/card";
 import { AddPlayerDialog } from "@/components/yatzy-lobby/AddPlayerDialog";
 import { GamemodePillSelector } from "@/components/yatzy-lobby/GamemodePillSelector";
 import { PlayerChip } from "@/components/yatzy-lobby/PlayerChip";
 import { RecentMatchesList } from "@/components/yatzy-lobby/RecentMatchesList";
 import { ArrowLeft, Dices } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -42,22 +40,15 @@ export default function YatzyLobby() {
 
   return (
     <>
-      <div className="w-full sticky dark:bg-[#0a0a0a] bg-white h-25 right-0 top-0">
-        <Card className="mx-4 p-4 flex flex-row items-center sticky top-4 z-30">
+      <div className="w-full sticky bg-background right-0 top-0">
+        <div className="mx-4 p-4 flex flex-row items-center sticky top-4 z-30">
           <Link href="/" className="flex flex-row items-center">
             <ArrowLeft className="mr-2" size={20} />
-            <Image
-              src="/images/dice.png"
-              alt="Dice"
-              width={512}
-              height={512}
-              className="w-8 h-8"
-            />
-            <h1 className="scroll-m-20 sm:text-2xl mb-1 ml-4 font-extrabold tracking-tight lg:text-3xl text-xl">
+            <h1 className="scroll-m-20 sm:text-2xl mb-1 font-extrabold tracking-tight lg:text-3xl text-xl">
               Yatzy
             </h1>
           </Link>
-        </Card>
+        </div>
       </div>
       <div className="flex flex-col gap-6 px-4 pb-8">
         <div className="flex flex-col gap-2">
